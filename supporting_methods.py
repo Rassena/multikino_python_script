d0dd57e9fb98761c647810c52139906779711d95
ksproska <kamila.sproska@gmail.com>
2021-11-03 09:36:44 +0100

    sql_addable contains string needed for self.mycursor.execute(...)

--- supporting_methods.py
+++ supporting_methods.py
@@ -40,9 +40,15 @@ class ObjectWithCounter:
 
 
 class AddableToDatabase:
+    @property
+    def sql_addable(self):
+        keys = ', '.join(self.__dict__.keys())
+        keys_insertable = ', '.join([f'%({key})s' for key in self.__dict__.keys()])
+        return f'INSERT INTO {self.__class__.__name__} ({keys}) VALUES ({keys_insertable})'
+
     def __str__(self):
         keys, values = '', ''
         if len(self.__dict__) > 0:
             keys = ', '.join(self.__dict__.keys())
-            values = ', '.join([f'%({key})s' for key in self.__dict__.keys()])
-        return f'INSERT INTO {self.__class__.__name__} ({keys}) VALUES ({values})'
+            values = ', '.join([str(value) for value in self.__dict__.values()])
+        return f'{self.__class__.__name__} ({keys}) : ({values})'
