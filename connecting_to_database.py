90da70cdbf39f011befb52915d51d4f8a08c6978
ksproska <kamila.sproska@gmail.com>
2021-11-03 09:59:33 +0100

    comments

--- connecting_to_database.py
+++ connecting_to_database.py
@@ -8,24 +8,21 @@ class Connection:
         self.mydb = mysql.connector.connect(host=host_ip, user="root", password="pdb_2021", database="multikino")
         self.mycursor = self.mydb.cursor()
 
-    def add_addable_element(self, addable_element: AddableToDatabase):
-        self.mycursor.execute(addable_element.sql_addable, addable_element.__dict__)
-        self.mydb.commit()
+    def add_addable_element(self, addable_element: AddableToDatabase) -> None:
+        """
+        Method for adding entity to database.
+        """
 
-    # def add_users(self, users_array):
-    #     sql = "INSERT INTO User (User_name, User_surname, User_email, user_password, User_birth) VALUES (%s, %s, %s, %s, %s)"
-    #     for u in users_array:
-    #         val = (u.User_name, u.User_surname, u.User_email, u.User_password, u.User_birth)
-    #         self.mycursor.execute(sql, val)
-    #         self.mydb.commit()
-    #
-    #
-    # def add_artists(self, artist_array):
-    #     sql = "INSERT INTO Artist (Artist_name, Artist_surname, Artist_birth, Artist_gender) VALUES (%s, %s, %s, %s)"
-    #     for a in artist_array:
-    #         val = (a.Artist_name, a.Artist_surname, a.Artist_birth, a.Artist_gender)
-    #         self.mycursor.execute(sql, val)
-    #         self.mydb.commit()
+        """
+        addable_element.sql_addable is a string in a form of:
+            "INSERT INTO ClassName (Atr_1, Atr_2) VALUES (%(Atr_1)s, %(Atr_2)s)"
+        
+        addable_element.__dict__ is a dictionary in a form of:
+            {'Atr_1': val1, 'Atr_2': val2}
+        """
+        self.mycursor.execute(addable_element.sql_addable, addable_element.__dict__)  # this should work, analogical to:
+        # https://dev.mysql.com/doc/connector-python/en/connector-python-example-cursor-transaction.html
+        self.mydb.commit()
 
 
 if __name__ == "__main__":
