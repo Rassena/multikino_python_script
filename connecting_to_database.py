81c6d979bdcf5dedff7c4de4dc2afaf7d2dca9dd
ksproska <kamila.sproska@gmail.com>
2021-11-03 09:37:37 +0100

    replaced with singular adding method

--- connecting_to_database.py
+++ connecting_to_database.py
@@ -13,21 +13,24 @@ class Connection():
         self.mydb = mysql.connector.connect(host="25.89.241.139", user="root", password="pdb_2021", database="multikino")
         self.mycursor = self.mydb.cursor()
 
+    def add_addable_element(self, addable_element: AddableToDatabase):
+        self.mycursor.execute(addable_element.sql_addable, addable_element.__dict__)
+        self.mydb.commit()
 
-    def add_users(self, users_array):
-        sql = "INSERT INTO User (User_name, User_surname, User_email, user_password, User_birth) VALUES (%s, %s, %s, %s, %s)"
-        for u in users_array:
-            val = (u.User_name, u.User_surname, u.User_email, u.User_password, u.User_birth)
-            self.mycursor.execute(sql, val)
-            self.mydb.commit()
-
-
-    def add_artists(self, artist_array):
-        sql = "INSERT INTO Artist (Artist_name, Artist_surname, Artist_birth, Artist_gender) VALUES (%s, %s, %s, %s)"
-        for a in artist_array:
-            val = (a.Artist_name, a.Artist_surname, a.Artist_birth, a.Artist_gender)
-            self.mycursor.execute(sql, val)
-            self.mydb.commit()
+    # def add_users(self, users_array):
+    #     sql = "INSERT INTO User (User_name, User_surname, User_email, user_password, User_birth) VALUES (%s, %s, %s, %s, %s)"
+    #     for u in users_array:
+    #         val = (u.User_name, u.User_surname, u.User_email, u.User_password, u.User_birth)
+    #         self.mycursor.execute(sql, val)
+    #         self.mydb.commit()
+    #
+    #
+    # def add_artists(self, artist_array):
+    #     sql = "INSERT INTO Artist (Artist_name, Artist_surname, Artist_birth, Artist_gender) VALUES (%s, %s, %s, %s)"
+    #     for a in artist_array:
+    #         val = (a.Artist_name, a.Artist_surname, a.Artist_birth, a.Artist_gender)
+    #         self.mycursor.execute(sql, val)
+    #         self.mydb.commit()
 
 
 if __name__ == "__main__":
