c19935145d0295ed6f29831bb5d9c59e0a0b0486
ksproska <kamila.sproska@gmail.com>
2021-11-03 11:32:09 +0100

    comments

--- supporting_methods.py
+++ supporting_methods.py
@@ -42,6 +42,9 @@ class ObjectWithCounter:
 class AddableToDatabase:
     @property
     def sql_addable(self):
+        """
+        example: "INSERT INTO ClassName (Atr_1, Atr_2) VALUES (%(Atr_1)s, %(Atr_2)s)"
+        """
         keys = ', '.join(self.__dict__.keys())
         keys_insertable = ', '.join([f'%({key})s' for key in self.__dict__.keys()])
         return f'INSERT INTO {self.__class__.__name__} ({keys}) VALUES ({keys_insertable})'
