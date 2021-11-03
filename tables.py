c908f00417dd886e0d25b2cadcab3f62fc167a8a
ksproska <kamila.sproska@gmail.com>
2021-11-03 07:52:31 +0100

    added counter class to inherit from

--- tables.py
+++ tables.py
@@ -31,58 +31,51 @@ def random_time_generator() -> time:
     pass
 
 
-class AgeRestriction:
-    AgeRestriction_counter = 0
+class ObjectWithCounter:
+    counter = 0
 
+
+class AgeRestriction(ObjectWithCounter):
     def __init__(self, age_restriction_name: str):
-        self.Id_ageRestriction: int = AgeRestriction.AgeRestriction_counter
-        AgeRestriction.AgeRestriction_counter += 1
+        self.Id_ageRestriction: int = AgeRestriction.counter
+        AgeRestriction.counter += 1
         self.AgeRestriction_name: str = age_restriction_name
 
 
-class Room:
-    Room_counter = 0
-
+class Room(ObjectWithCounter):
     def __init__(self, room_sign: str):
-        self.Id_room: int = Room.Room_counter
-        Room.Room_counter += 1
+        self.Id_room: int = Room.counter
+        Room.counter += 1
         self.Room_sign: str = room_sign
 
 
-class ReservationState:
-    ReservationState_counter = 0
-
+class ReservationState(ObjectWithCounter):
     def __init__(self, reservation_state_name: str):
-        self.Id_reservationState: int = ReservationState.ReservationState_counter
-        ReservationState.ReservationState_counter += 1
+        self.Id_reservationState: int = ReservationState.counter
+        ReservationState.counter += 1
         self.ReservationState_name: str = reservation_state_name
 
 
-class Role:
-    Role_counter = 0
-
+class Role(ObjectWithCounter):
     def __init__(self, role_name: str):
-        self.Id_role: int = Role.Role_counter
-        Role.Role_counter += 1
+        self.Id_role: int = Role.counter
+        Role.counter += 1
         self.Role_name: str = role_name
 
 
-class Translation:
-    Translation_counter = 0
-
+class Translation(ObjectWithCounter):
     def __init__(self, translation_name: str):
-        self.Id_translation: int = Translation.Translation_counter
-        Translation.Translation_counter += 1
+        self.Id_translation: int = Translation.counter
+        Translation.counter += 1
         self.Translation_name = translation_name
 
 
-class Artist:
-    Artist_counter = 0
+class Artist(ObjectWithCounter):
     genders = {False: 'male', True: 'female'}
 
     def __init__(self):
-        self.Id_artist: int = Artist.Artist_counter
-        Artist.Artist_counter += 1
+        self.Id_artist: int = Artist.counter
+        Artist.counter += 1
         gender_id = random.choice([False, True])
         self.Artist_name: str = names.get_first_name(gender=self.genders[gender_id])
         self.Artist_surname: str = names.get_last_name()
@@ -90,13 +83,12 @@ class Artist:
         self.Artist_gender: bool = gender_id
 
 
-class User:
-    User_counter = 0
+class User(ObjectWithCounter):
     genders = {False: 'male', True: 'female'}
 
     def __init__(self):
-        self.Id_use: int = User.User_counter
-        User.User_counter += 1
+        self.Id_use: int = User.counter
+        User.counter += 1
         gender_id = random.choice([False, True])
         self.User_name: str = names.get_first_name(gender=self.genders[gender_id])
         self.User_surname: str = names.get_last_name()
@@ -105,14 +97,13 @@ class User:
         self.User_birth: date = random_date_generator()
 
 
-class Seat:
-    Seat_counter = 0
+class Seat(ObjectWithCounter):
     MAX_SEAT_IN_ROW = 15
     rooms_seats = {}
 
     def __init__(self, fk_room: int):
-        self.Id_seat: int = Seat.Seat_counter
-        Seat.Seat_counter += 1
+        self.Id_seat: int = Seat.counter
+        Seat.counter += 1
 
         if fk_room not in Seat.rooms_seats.keys():
             Seat.rooms_seats[fk_room] = 0
@@ -125,12 +116,10 @@ class Seat:
         self.Fk_room: int = fk_room
 
 
-class Movie:
-    Movie_counter = 0
-
+class Movie(ObjectWithCounter):
     def __init__(self, fk_age_restriction: int):
-        self.Id_movie: int = Movie.Movie_counter
-        Movie.Movie_counter += 1
+        self.Id_movie: int = Movie.counter
+        Movie.counter += 1
 
         self.Movie_name: str = f'Movie name {random_string_generator(5)}'
         self.Movie_description: str = random_string_generator(100)
@@ -140,159 +129,127 @@ class Movie:
         self.Fk_ageRestriction: int = fk_age_restriction
 
 
-class CastAssignment:
-    CastAssignment_counter = 0
-
+class CastAssignment(ObjectWithCounter):
     def __init__(self, fk_artist, fk_movie, fk_role):
-        self.Id_castAssignment: int = CastAssignment.CastAssignment_counter
-        CastAssignment.CastAssignment_counter += 1
+        self.Id_castAssignment: int = CastAssignment.counter
+        CastAssignment.counter += 1
         self.Fk_artist: int = fk_artist
         self.Fk_movie: int = fk_movie
         self.Fk_role: int = fk_role
 
 
-class Country:
-    Country_counter = 0
-
+class Country(ObjectWithCounter):
     def __init__(self, country_name: str):
-        self.Id_country = Country.Country_counter
-        Country.Country_counter += 1
+        self.Id_country = Country.counter
+        Country.counter += 1
         self.Country_name: str = country_name
 
 
-class Dimension:
-    Dimension_counter = 0
-
+class Dimension(ObjectWithCounter):
     def __init__(self, dimension_name: str):
-        self.Id_dimension: int = Dimension.Dimension_counter
-        Dimension.Dimension_counter += 1
+        self.Id_dimension: int = Dimension.counter
+        Dimension.counter += 1
         self.Dimension_name: str = dimension_name
 
 
-class Discount:
-    Discount_counter = 0
-
+class Discount(ObjectWithCounter):
     def __init__(self, discount_name: str):
-        self.Id_discount = Discount.Discount_counter
-        Discount.Discount_counter += 1
+        self.Id_discount = Discount.counter
+        Discount.counter += 1
         self.Discount_name: str = discount_name
 
 
-class Genre:
-    Genre_counter = 0
-
+class Genre(ObjectWithCounter):
     def __init__(self, genre_name: str):
-        self.Id_genre: int = Genre.Genre_counter
-        Genre.Genre_counter += 1
+        self.Id_genre: int = Genre.counter
+        Genre.counter += 1
         self.Genre_name: str = genre_name
 
 
-class MovieVersion:
-    MovieVersion_counter = 0
-
+class MovieVersion(ObjectWithCounter):
     def __init__(self, fk_translation: int, fk_dimension: int):
-        self.Id_movieVersion: int = MovieVersion.MovieVersion_counter
-        MovieVersion.MovieVersion_counter += 1
+        self.Id_movieVersion: int = MovieVersion.counter
+        MovieVersion.counter += 1
         self.Fk_translation = fk_translation
         self.Fk_dimension = fk_dimension
 
 
-class Movie_Country:
-    Movie_Country_counter = 0
-
+class Movie_Country(ObjectWithCounter):
     def __init__(self, fk_movie: int, fk_country: int):
-        self.Id_movie_country: int = Movie_Country.Movie_Country_counter
-        Movie_Country.Movie_Country_counter += 1
+        self.Id_movie_country: int = Movie_Country.counter
+        Movie_Country.counter += 1
         self.Fk_movie = fk_movie
         self.Fk_country = fk_country
 
 
-class Movie_Genre:
-    Movie_Genre_counter = 0
-
+class Movie_Genre(ObjectWithCounter):
     def __init__(self, fk_movie: int, fk_genre: int):
-        self.Id_movie_genre: int = Movie_Genre.Movie_Genre_counter
-        Movie_Genre.Movie_Genre_counter += 1
+        self.Id_movie_genre: int = Movie_Genre.counter
+        Movie_Genre.counter += 1
         self.Fk_movie = fk_movie
         self.Fk_genre = fk_genre
 
 
-class Movie_MovieVersion:
-    Movie_MovieVersion_counter = 0
-
+class Movie_MovieVersion(ObjectWithCounter):
     def __init__(self, fk_movie: int, fk_movieVersion: int):
-        self.Id_movie_movieVersion: int = Movie_MovieVersion.Movie_MovieVersion_counter
-        Movie_MovieVersion.Movie_MovieVersion_counter += 1
+        self.Id_movie_movieVersion: int = Movie_MovieVersion.counter
+        Movie_MovieVersion.counter += 1
         self.Fk_movie = fk_movie
         self.Fk_movieVersion = fk_movieVersion
 
 
-class Poster:
-    Poster_counter = 0
-
+class Poster(ObjectWithCounter):
     def __init__(self, fk_movie: int):
-        self.Id_poster: int = Poster.Poster_counter
-        Poster.Poster_counter += 1
+        self.Id_poster: int = Poster.counter
+        Poster.counter += 1
         self.Fk_movie = fk_movie
         self.Poster_path = f'C:\posters\{self.Fk_movie}\{random_string_generator(4)}.tiff'
 
 
-class Price:
-    Price_counter = 0
-
+class Price(ObjectWithCounter):
     def __init__(self, price_value: float):
-        self.Id_price = Price.Price_counter
-        Price.Price_counter += 1
+        self.Id_price = Price.counter
+        Price.counter += 1
         self.Price_value: float = price_value
 
 
-class Privilege:
-    Privlege_counter = 0
-
+class Privilege(ObjectWithCounter):
     def __init__(self, privilege_name: str):
-        self.Id_privilege: int = Privilege.Privlege_counter
-        Privilege.Privlege_counter += 1
+        self.Id_privilege: int = Privilege.counter
+        Privilege.counter += 1
         self.Privilege_name = privilege_name
 
 
-class PrivilegeAssignment:
-    PrivilegeAssignment_counter = 0
-
+class PrivilegeAssignment(ObjectWithCounter):
     def __init__(self, fk_user: int, fk_privilege: int):
-        self.Id_privilegeAssignment: int = PrivilegeAssignment.PrivilegeAssignment_counter
-        PrivilegeAssignment.PrivilegeAssignment_counter += 1
+        self.Id_privilegeAssignment: int = PrivilegeAssignment.counter
+        PrivilegeAssignment.counter += 1
         self.Fk_user = fk_user
         self.Fk_privilege = fk_privilege
 
 
-class Rating:
-    Rating_counter = 0
-
+class Rating(ObjectWithCounter):
     def __init__(self, fk_movie: int, fk_user: int):
-        self.Id_rating: int = Rating.Rating_counter
-        Rating.Rating_counter += 1
+        self.Id_rating: int = Rating.counter
+        Rating.counter += 1
         self.Fk_movie = fk_movie
         self.Fk_user = fk_user
         self.Rating_rate = random.randint(1, 10) # 1 <= Rating_rate <= 10)
 
 
-class Seans:
-    Seans_counter = 0
-
+class Seans(ObjectWithCounter):
     def __init__(self, fk_room: int, fk_movieVersion: int):
-        self.Id_seans: int = Seans.Seans_counter
-        Seans.Seans_counter += 1
+        self.Id_seans: int = Seans.counter
+        Seans.counter += 1
         self.Fk_room = fk_room
         self.Fk_movieVersion = fk_movieVersion
         self.Seans_date = random_date_generator()
 
 
-class Reservation:
-    Reservation_count = 0
-
+class Reservation(ObjectWithCounter):
     def __init__(self, fk_seans: int, fk_reservationState: int, fk_user: int):
-        self.Id_reservation: int = Reservation.Reservation_count
-        Reservation.Reservation_count += 1
+        self.Id_reservation: int = Reservation.counter
+        Reservation.counter += 1
         self.Fk_seans = fk_seans
         self.Fk_reservationState = fk_reservationState
         self.Fk_user = fk_user
