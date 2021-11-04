2ff7c92b81423d99bd30ab253d73ae5409b44f2b
ksproska <kamila.sproska@gmail.com>
2021-11-04 23:23:58 +0100

    cmd command lines - do not run this file

---
+++ sql_cmd_common.bat
@@ -0,0 +1,11 @@
+:: do not run this, it only contains random command lines
+
+:: michala:
+mysql multikino -h 25.89.241.139 -u root -p
+
+:: virtual machine:
+mysql multikino --host=25.3.64.93 --user=root -p
+:: password can also be in one line: --password=and_there_password
+
+:: clear screan in mysql
+\! cls
