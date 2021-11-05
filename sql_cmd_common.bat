ea7534e09649396edc7e0112f9f3edb27f6b3d4c
ksproska <kamila.sproska@gmail.com>
2021-11-05 03:06:35 +0100

    choco commends, correction for ip

--- sql_cmd_common.bat
+++ sql_cmd_common.bat
@@ -1,10 +1,14 @@
 :: do not run this, it only contains random command lines
+:: install choco in powershell
+:: Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+:: install mysql using choco
+choco install mysql
 
 :: michala:
 mysql multikino -h 25.89.241.139 -u root --password=pdb_2021
 
 :: virtual machine:
-mysql multikino --host=25.89.241.139 --user=root --password=pdb_2021
+mysql multikino --host=25.3.64.93 --user=root --password=pdb_2021
 
 :: clear screan in mysql
 \! cls
