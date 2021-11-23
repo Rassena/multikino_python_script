5f5d3de844fa7b6e2ef77f4f3334ba8481f2a5c3
ksproska <kamila.sproska@gmail.com>
2021-11-23 11:01:06 +0100

    understanding explain notes and creating indexes first draft

---
+++ creating_indexes.sql
@@ -0,0 +1,42 @@
+/*
++ częste wyszukiwanie seansow po dacie (szczegolnie = DATE(NOW())) (Top 10.1)
+- dodawanie seansow do bazy danych odbywa sie codziennie (Top 10.10)
+*/
+CREATE INDEX seans_date_idx
+ON seans(Seans_date)
+;
+
+/*
++ czeste sortowanie po czasie seansu (Top 10.5)
++ czeste porownywanie czasow (szczegolnie <= TIME(NOW())) (Top 10.1)
+- dodawanie seansow do bazy danych odbywa sie codziennie (Top 10.10)
+*/
+CREATE INDEX seans_time_idx
+ON seans(Seans_time)
+;
+
+/*
++ czeste wyszukiwana rezerwacji po nazwisku (Top 10.1)
+- dodawanie uzytkownikow do bazy danych odbywa sie codziennie, lecz bez regularnosci (Top 10.10)
+*/
+CREATE INDEX user_surname_inx
+ON user(User_surname)
+;
+
+/*
++ czeste porownywania stanow rezerwacji
++ brak modyfikacji (Top 10.10)
+- malo elementow
+*/
+CREATE INDEX reservationstate_id_inx
+ON reservationstate(Id_reservationState)
+;
+
+/*
++ czeste filtrowanie po gatunkach
++ praktycznie brak modyfikacji (Top 10.10)
++ stosunkowo duzo elementow
+*/
+CREATE INDEX genre_id_inx
+ON genre(Id_genre)
+;
--- queries.sql
+++ queries.sql
@@ -1,13 +1,11 @@
 /* Query 1 (interface 16) - wszystkie seanse dzisiaj posortowane rosnąco po godzinie,
    w tabeli ma być informacja o tłumaczeniu i wymiarze seansu*/
+# EXPLAIN
 SELECT Movie_name, Seans_time, (Seans_time <= NOW()) AS 'Has_started', Translation_name, Dimension_name
 FROM seans
-INNER JOIN movie_movieversion
-ON seans.Fk_movie_MovieVersion = movie_movieversion.Id_movie_movieVersion
-INNER JOIN movie
-ON movie_movieversion.Fk_movie = movie.Id_movie
-INNER JOIN movieversion m
-    on movie_movieversion.Fk_movieVersion = m.Id_movieVersion
+INNER JOIN movie_movieversion ON seans.Fk_movie_MovieVersion = movie_movieversion.Id_movie_movieVersion
+INNER JOIN movie ON movie_movieversion.Fk_movie = movie.Id_movie
+INNER JOIN movieversion m on movie_movieversion.Fk_movieVersion = m.Id_movieVersion
 INNER JOIN dimension d on m.Fk_dimension = d.Id_dimension
 INNER JOIN translation t on m.Fk_translation = t.Id_translation
 WHERE seans.Seans_date = DATE(NOW())
@@ -15,6 +13,7 @@ ORDER BY seans.Seans_time;
 
 
 /* Query 2 (interface 9, 17) - wszystkie zajęte miejsca dla danego seansu */
+# EXPLAIN
 SELECT seans.Id_seans, m.Movie_name, s.Seat_row, s.Seat_number, rs.ReservationState_name
 FROM seans
 INNER JOIN reservation ON seans.Id_seans = reservation.Fk_seans
@@ -29,10 +28,10 @@ AND (rs.Id_reservationState = 2 OR
 
 
 /* Query 3 (interface 19) - wszystkie dzisiejsze rezerwacje na dane nazwisko */
+# EXPLAIN
 SELECT user.User_surname, m.Movie_name, seans.Seans_time, ReservationState_name, COUNT(Id_ticket) AS 'Number_of_tickets'
 FROM reservation
-INNER JOIN seans
-ON reservation.Fk_seans = seans.Id_seans
+INNER JOIN seans ON reservation.Fk_seans = seans.Id_seans
 INNER JOIN user on reservation.Fk_user = user.Id_user
 INNER JOIN reservationstate r on reservation.Fk_reservationState = r.Id_reservationState
 INNER JOIN ticket t on reservation.Id_reservation = t.Fk_reservation
@@ -44,10 +43,10 @@ GROUP BY Id_reservation;
 
 # TODO: sprawdzić ponownie czy ceny są poprawnie wprowadzone (inne cany na ten sam seans)
 /* Query 4 (interface 14) - cena do zapłaty za rezerwację */
+# EXPLAIN
 SELECT Id_reservation, SUM(Price_value*(1- Discount_value)) AS Ticket_price
 FROM reservation
-INNER JOIN ticket
-ON reservation.Id_reservation = ticket.Fk_reservation
+INNER JOIN ticket ON reservation.Id_reservation = ticket.Fk_reservation
 INNER JOIN price p on ticket.Fk_price = p.Id_price
 INNER JOIN discount d on ticket.Fk_discount = d.Id_discount
 INNER JOIN seat s on ticket.Fk_seat = s.Id_seat
@@ -56,6 +55,7 @@ WHERE Id_reservation = 696 # 696, 5
 
 /* Query 5 (interface 1, 2, 3, 6) - najpopularniejsze filmy sortowane po największej
    ilości sprzedanych biletów, wyświetlamy tytuły dostępne tylko w danym dniu - do zrobienia */
+# EXPLAIN
 SELECT  m.Movie_name, Seans_date, gr.count
 FROM seans
 INNER JOIN movie_movieversion mmv on seans.Fk_movie_MovieVersion = mmv.Id_movie_movieVersion
@@ -73,6 +73,7 @@ WHERE Seans_date = DATE(NOW())
 ORDER BY gr.count DESC;
 
 /* Query 6 (interface 3, 4, 6) - liczenie średniej ratingu dla filmów i wyświetlenie w kolejności od najlepszego */
+# EXPLAIN
 SELECT m.Movie_name, AVG(Rating_rate) AS 'Rate'
 FROM rating
 INNER JOIN movie m on rating.Fk_movie = m.Id_movie
@@ -82,10 +83,10 @@ ORDER BY Rate DESC;
 
 # ??
 /* Query 7 (interface 19) - czy można odebrać rezerwację (jeśli miejsca nie są wykupione to można) - do zrobienia */
+# EXPLAIN
 SELECT reservation.Id_reservation, Id_seat, (Id_reservationState != 3) AS 'Is_buyable'
 FROM reservation
-INNER JOIN seans
-ON reservation.Fk_seans = seans.Id_seans
+INNER JOIN seans ON reservation.Fk_seans = seans.Id_seans
 INNER JOIN user on reservation.Fk_user = user.Id_user
 INNER JOIN reservationstate r on reservation.Fk_reservationState = r.Id_reservationState
 INNER JOIN ticket t on reservation.Id_reservation = t.Fk_reservation
@@ -95,6 +96,7 @@ INNER JOIN seat s on t.Fk_seat = s.Id_seat
 WHERE reservation.Id_reservation = :input_id_reservation; #696, 50, 127
 
 /* Query 8 (interface 16) - lista obecnie trwających seansów (mamy datę rozpoczęcia i czas trwania filmu)*/
+# EXPLAIN
 SELECT m.Movie_name, Seans_time, (Seans_time <= TIME(NOW())) AS 'Rozpoczal sie', t.Translation_name, d.Dimension_name
 FROM seans
 INNER JOIN movie_movieversion mmv on seans.Fk_movie_MovieVersion = mmv.Id_movie_movieVersion
@@ -106,6 +108,7 @@ WHERE Seans_date = DATE(NOW())
 ORDER BY Seans_time, m.Movie_name;
 
 /* Query 9 (interface ) - lista gatunków dla danego tytułu - do zrobienia */
+# EXPLAIN
 SELECT Genre_name
 FROM movie_genre
 INNER JOIN genre g on movie_genre.Fk_genre = g.Id_genre
@@ -114,6 +117,7 @@ WHERE Id_movie = :input_id_movie; #46, 120
 
 /* Query 10 (interface 5) - lista filmów w którym dany twórca brał udział,
    w tabeli informacja o roli (reżyser/aktor/scenarzysta)*/
+# EXPLAIN
 SELECT movie.Movie_name, r.Role_name
 FROM movie
 INNER JOIN castassignment c on movie.Id_movie = c.Fk_movie
@@ -123,6 +127,7 @@ WHERE a.Id_artist = :input_id_artist # 4, 6, 10
 ORDER BY r.Role_name, Movie_name;
 
 /* Query 11 (interface 8) - Wszystkie bieżące rezerwacje dla danego konta - do zrobienia */
+# EXPLAIN
 SELECT Seans_date, Seans_time, m.Movie_name
 FROM seans
 INNER JOIN movie_movieversion mmv on seans.Fk_movie_MovieVersion = mmv.Id_movie_movieVersion
@@ -136,6 +141,7 @@ AND u.Id_user = :input_id_user # 572, 663, 610
 ;
 
 /* Query 12 (interface 4) - Wyświetlanie repertuaru w wybrany dzień dla danego filmu */
+# EXPLAIN
 SELECT Seans_time, d.Dimension_name, t.Translation_name
 FROM seans
 INNER JOIN movie_movieversion mmv on seans.Fk_movie_MovieVersion = mmv.Id_movie_movieVersion
